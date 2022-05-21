fad5c82ae5124b6da9b3a9951db8b5d270b02be0
herkinds <58661318+herkinds@users.noreply.github.com>
2022-05-21 21:18:50 +0200

    Added an interface for random number generation

---
+++ src/Stochastics/RandomNumberGenerator/IRandomNumberGenerator.cs
@@ -0,0 +1,16 @@
+﻿namespace Herkinds.InsuranceMath.Stochastics.RandomNumberGenerator
+{
+    /// <summary>
+    /// A pseudo-random number generator interface.
+    /// </summary>
+    /// <typeparam name="TNumber">The type of number that can be generated.</typeparam>
+    public interface IRandomNumberGenerator<out TNumber>
+        where TNumber : struct
+    {
+        /// <summary>
+        /// Generates a pseudo-random number from the domain.
+        /// </summary>
+        /// <returns>A pseudo-random number.</returns>
+        TNumber Generate();
+    }
+}
