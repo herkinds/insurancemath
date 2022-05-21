7adb2cd34b3454b3db71fd3f635af4a1c4cbd2da
herkinds <58661318+herkinds@users.noreply.github.com>
2022-05-21 11:20:14 +0200

    Added the first set up for IDistribution

---
+++ src/Stochastics/Distributions/IDistribution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Herkinds.InsuranceMath.Stochastics.Distributions
+{
+    /// <summary>
+    /// A probability distribution.
+    /// </summary>
+    /// <typeparam name="TDomain">The type of the domain on which the probability distribution is defined.</typeparam>
+    public interface IDistribution<in TDomain>
+        where TDomain : IComparable<TDomain>
+    {
+        /// <summary>
+        /// Computes the cumulative distribution function.
+        /// </summary>
+        /// <param name="boundary">The boundary.</param>
+        /// <returns>The probability.</returns>
+        Probability CumulativeDistributionFunction(TDomain boundary);
+    }
+}
