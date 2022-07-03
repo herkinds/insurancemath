fda0e5a3218abff85f580093ba84ab8b084ba38e
herkinds <58661318+herkinds@users.noreply.github.com>
2022-07-03 09:47:57 +0200

    Added more tests for Probability.Complement

--- test/Stochastics.Tests/ProbabilityTest.cs
+++ test/Stochastics.Tests/ProbabilityTest.cs
@@ -10,5 +10,26 @@ namespace Stochastics.Tests
 
             Assert.Equal(expected, actual);
         }
+
+        [Fact]
+        public void Complement_One_Zero()
+        {
+            var actual = Probability.One.Complement();
+            var expected = Probability.Zero;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(0.5, 0.5)]
+        [InlineData(0.75, 0.25)]
+        [InlineData(0.25, 0.75)]
+        [InlineData(0.1, 0.9)]
+        [InlineData(0.0001, 0.9999)]
+        public void Complement_Values_(decimal probability, decimal expected)
+        {
+            var actual = Probability.Parse(probability).Complement();
+            Assert.Equal(expected, actual);
+        }
     }
 }
